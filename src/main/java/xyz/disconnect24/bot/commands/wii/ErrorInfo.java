1543329218088d2c641b5404ff90ee520872e7ab
Spotlight <spotlight@joscomputing.space>
2018-04-08 04:13:08 -0500

    Update copyright
    Related to https://github.com/RiiConnect24-Bot/RC24-Bot/pull/62

--- src/main/java/xyz/disconnect24/bot/commands/wii/ErrorInfo.java
+++ src/main/java/xyz/disconnect24/bot/commands/wii/ErrorInfo.java
@@ -3,7 +3,7 @@ package xyz.disconnect24.bot.commands.wii;
 /*
  * The MIT License
  *
- * Copyright 2017 Artu.
+ * Copyright 2017 Disconnect24 and its contributors.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
